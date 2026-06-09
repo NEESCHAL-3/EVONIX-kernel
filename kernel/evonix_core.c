8d138dde5f99af3c24ebfbb73d2a58d48106ccb1
NEESCHAL-3 <Nischalkhadka022@gmail.com>
2026-06-09 14:11:11 +0545

    EVONIX: add charging sysfs API skeleton
    Change-Id: I34365bddd9de1b72de955e1adc33743c8b4678ed

--- kernel/evonix_core.c
+++ kernel/evonix_core.c
@@ -7,6 +7,8 @@
 #include <linux/kernel.h>
 #include <linux/kobject.h>
 #include <linux/sysfs.h>
+#include <linux/mutex.h>
+#include <linux/string.h>
 
 #define EVONIX_API_VERSION	"1"
 #define EVONIX_RELEASE		"v3.0"
@@ -15,6 +17,12 @@
 #define EVONIX_BASE		"6.6 LTS"
 
 static struct kobject *evonix_kobj;
+static struct kobject *evonix_charging_kobj;
+
+static DEFINE_MUTEX(evonix_charging_lock);
+static int evonix_limit_enabled;
+static int evonix_limit_percent = 80;
+static char evonix_watt_mode[16] = "dynamic";
 
 static ssize_t api_version_show(struct kobject *kobj,
 				struct kobj_attribute *attr, char *buf)
@@ -51,12 +59,155 @@ static ssize_t features_show(struct kobject *kobj,
 {
 	return sysfs_emit(buf,
 			  "identity=1\n"
-			  "charging=0\n"
-			  "watt_mode=0\n"
+			  "charging_api=1\n"
+			  "charge_limit=api_only\n"
+			  "watt_mode=api_only\n"
 			  "bypass=0\n"
 			  "performance=0\n"
 			  "touch=0\n");
 }
+
+
+static ssize_t limit_enabled_show(struct kobject *kobj,
+				  struct kobj_attribute *attr, char *buf)
+{
+	int val;
+
+	mutex_lock(&evonix_charging_lock);
+	val = evonix_limit_enabled;
+	mutex_unlock(&evonix_charging_lock);
+
+	return sysfs_emit(buf, "%d\n", val);
+}
+
+static ssize_t limit_enabled_store(struct kobject *kobj,
+				   struct kobj_attribute *attr,
+				   const char *buf, size_t count)
+{
+	int val;
+
+	if (kstrtoint(buf, 0, &val))
+		return -EINVAL;
+
+	if (val != 0 && val != 1)
+		return -EINVAL;
+
+	mutex_lock(&evonix_charging_lock);
+	evonix_limit_enabled = val;
+	mutex_unlock(&evonix_charging_lock);
+
+	return count;
+}
+
+static ssize_t limit_percent_show(struct kobject *kobj,
+				  struct kobj_attribute *attr, char *buf)
+{
+	int val;
+
+	mutex_lock(&evonix_charging_lock);
+	val = evonix_limit_percent;
+	mutex_unlock(&evonix_charging_lock);
+
+	return sysfs_emit(buf, "%d\n", val);
+}
+
+static ssize_t limit_percent_store(struct kobject *kobj,
+				   struct kobj_attribute *attr,
+				   const char *buf, size_t count)
+{
+	int val;
+
+	if (kstrtoint(buf, 0, &val))
+		return -EINVAL;
+
+	if (val < 1 || val > 100)
+		return -EINVAL;
+
+	mutex_lock(&evonix_charging_lock);
+	evonix_limit_percent = val;
+	mutex_unlock(&evonix_charging_lock);
+
+	return count;
+}
+
+static ssize_t watt_mode_show(struct kobject *kobj,
+			      struct kobj_attribute *attr, char *buf)
+{
+	char mode[16];
+
+	mutex_lock(&evonix_charging_lock);
+	strscpy(mode, evonix_watt_mode, sizeof(mode));
+	mutex_unlock(&evonix_charging_lock);
+
+	return sysfs_emit(buf, "%s\n", mode);
+}
+
+static ssize_t watt_mode_store(struct kobject *kobj,
+			       struct kobj_attribute *attr,
+			       const char *buf, size_t count)
+{
+	const char *mode = NULL;
+
+	if (sysfs_streq(buf, "dynamic"))
+		mode = "dynamic";
+	else if (sysfs_streq(buf, "33"))
+		mode = "33";
+	else if (sysfs_streq(buf, "45"))
+		mode = "45";
+	else if (sysfs_streq(buf, "65"))
+		mode = "65";
+	else if (sysfs_streq(buf, "90"))
+		mode = "90";
+	else
+		return -EINVAL;
+
+	mutex_lock(&evonix_charging_lock);
+	strscpy(evonix_watt_mode, mode, sizeof(evonix_watt_mode));
+	mutex_unlock(&evonix_charging_lock);
+
+	return count;
+}
+
+static ssize_t charging_status_show(struct kobject *kobj,
+				    struct kobj_attribute *attr, char *buf)
+{
+	int enabled;
+	int percent;
+	char mode[16];
+
+	mutex_lock(&evonix_charging_lock);
+	enabled = evonix_limit_enabled;
+	percent = evonix_limit_percent;
+	strscpy(mode, evonix_watt_mode, sizeof(mode));
+	mutex_unlock(&evonix_charging_lock);
+
+	return sysfs_emit(buf,
+			  "api=1\n"
+			  "enforcement=0\n"
+			  "limit_enabled=%d\n"
+			  "limit_percent=%d\n"
+			  "watt_mode=%s\n",
+			  enabled, percent, mode);
+}
+
+static struct kobj_attribute limit_enabled_attr = __ATTR_RW(limit_enabled);
+static struct kobj_attribute limit_percent_attr = __ATTR_RW(limit_percent);
+static struct kobj_attribute watt_mode_attr = __ATTR_RW(watt_mode);
+static struct kobj_attribute charging_status_attr =
+	__ATTR(status, 0444, charging_status_show, NULL);
+
+static struct attribute *evonix_charging_attrs[] = {
+	&limit_enabled_attr.attr,
+	&limit_percent_attr.attr,
+	&watt_mode_attr.attr,
+	&charging_status_attr.attr,
+	NULL,
+};
+
+static const struct attribute_group evonix_charging_attr_group = {
+	.attrs = evonix_charging_attrs,
+};
+
 
 static struct kobj_attribute api_version_attr = __ATTR_RO(api_version);
 static struct kobj_attribute release_attr = __ATTR_RO(release);
@@ -89,6 +240,24 @@ static int __init evonix_core_init(void)
 
 	ret = sysfs_create_group(evonix_kobj, &evonix_attr_group);
 	if (ret) {
+		kobject_put(evonix_kobj);
+		evonix_kobj = NULL;
+		return ret;
+	}
+
+	evonix_charging_kobj = kobject_create_and_add("charging", evonix_kobj);
+	if (!evonix_charging_kobj) {
+		sysfs_remove_group(evonix_kobj, &evonix_attr_group);
+		kobject_put(evonix_kobj);
+		evonix_kobj = NULL;
+		return -ENOMEM;
+	}
+
+	ret = sysfs_create_group(evonix_charging_kobj, &evonix_charging_attr_group);
+	if (ret) {
+		kobject_put(evonix_charging_kobj);
+		evonix_charging_kobj = NULL;
+		sysfs_remove_group(evonix_kobj, &evonix_attr_group);
 		kobject_put(evonix_kobj);
 		evonix_kobj = NULL;
 		return ret;
